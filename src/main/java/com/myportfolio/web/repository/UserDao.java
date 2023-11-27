5399854c85977c066db1f8cfd65cde3310010698
gd095529 <gd095529@naver.com>
2023-11-27 17:51:41 +0900

    modify configuration files

--- src/main/java/com/myportfolio/web/repository/UserDao.java
+++ src/main/java/com/myportfolio/web/repository/UserDao.java
@@ -1,6 +1,7 @@
 package com.myportfolio.web.repository;
 
 import com.myportfolio.web.dbset.User;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -9,21 +10,90 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public class UserDao {
     @Autowired
     DataSource ds;
 
-    public User selectUser(String id) throws Exception {
-        User user = null;
-        String sql = "select * from user_info where id= ? ";
+    @Autowired
+    SqlSession session;
 
+    String namespace = "com.myportfolio.web.repository.UserMapper.";
+//    public User select(String id) throws Exception {
+//        return session.select(namespace+"select", id);
+//    }
+
+    public int updateUser(User user) throws Exception {
+        String sql = "update user_info set pwd = ? , birth =? , name=?, email=? where id = ?";
+        int rowCnt = 0;
+        try(
+                Connection con = ds.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
+                ){
+            pstmt.setString(1, user.getPwd());
+            pstmt.setDate(2, (java.sql.Date) user.getBirth());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+            pstmt.setString(5, user.getId());
+
+            rowCnt = pstmt.executeUpdate();
+        }
+        return rowCnt;
+    }
+
+    public int countUser() throws Exception {
+        String sql = "select count(*) from user_info ";
+        int cnt = 0;
+        try(
+                Connection con = ds.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery();
+                ){
+            rs.next();
+            cnt = rs.getInt(1);
+        }
+        return cnt;
+    }
+
+    public int deleteAllUser() throws Exception {
+        String sql = "delete from user_info ";
+        int rowCnt = 0;
+        try(
+                Connection con = ds.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
+                ){
+            rowCnt = pstmt.executeUpdate();
+        }
+        return rowCnt;
+    }
+
+    public int insertUser(User user) throws Exception{
+        String sql = "insert into user_info values(?,?,?,?,?) ";
+        int rowCnt = 0;
 
         try(
                 Connection con = ds.getConnection();
                 PreparedStatement pstmt = con.prepareStatement(sql);
+                ){
+            pstmt.setString(1,user.getId());
+            pstmt.setString(2,user.getPwd());
+            pstmt.setDate(3, (java.sql.Date) user.getBirth());
+            pstmt.setString(4,user.getName());
+            pstmt.setString(5,user.getEmail());
 
+            rowCnt = pstmt.executeUpdate();
+        }
+        return rowCnt;
+    }
+
+    public User selectUser(String id) throws Exception {
+        User user = null;
+        String sql = "select * from user_info where id= ? ";
+        try(
+                Connection con = ds.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
                 ){
             pstmt.setString(1,id);
             ResultSet rs = pstmt.executeQuery();
@@ -31,12 +101,11 @@ public class UserDao {
                 user = new User();
                 user.setId(rs.getString("id"));
                 user.setPwd(rs.getString("pwd"));
-                user.setBirth(new Date(rs.getDate("birth").getDate()));
+                user.setBirth((java.sql.Date)rs.getDate("birth"));
                 user.setName(rs.getString("name"));
                 user.setEmail(rs.getString("email"));
             }
         }
-
         return user;
     }
 
--- src/test/java/com/myportfolio/web/repository/UserDaoTest.java
+++ src/test/java/com/myportfolio/web/repository/UserDaoTest.java
@@ -17,9 +17,51 @@ public class UserDaoTest {
     private UserDao userDao;
 
     @Test
-    public void selectUserTest() throws Exception {
-        User user = userDao.selectUser("gd095529");
+    public void updateUserTest() throws Exception {
+        userDao.deleteAllUser();
+        User user = new User("asdf","asdf",null,"asdf","asdf");
+        userDao.insertUser(user);
+        User user2 = userDao.selectUser("asdf");
+        int cnt = userDao.updateUser(new User("asdf","1234",null,"qwer","zxcv"));
+        assertTrue(cnt != 0);
+    }
+    @Test
+    public void CountUser() throws Exception {
+        userDao.deleteAllUser();
+        int cnt = 0;
+        User user = new User("asdf","asdf",null,"asdf","asdf");
+        userDao.insertUser(user);
+        cnt = userDao.countUser();
+        assertTrue(cnt==1);
+    }
 
-        assertTrue(user.getId().equals("gd095529"));
+    @Test
+    public void deleteAllUserTest() throws Exception {
+        userDao.deleteAllUser();
+        User user = new User("asdf","asdf",null,"asdf","asdf");
+        userDao.insertUser(user);
+        assertTrue(userDao.countUser()==1);
+        userDao.deleteAllUser();
+
+        assertTrue(userDao.countUser()==0);
+    }
+
+    @Test
+    public void insertUserTest() throws Exception{
+        userDao.deleteAllUser();
+        User user = new User("test","1234",null,"newone","aaa@naver.com");
+        int rowCnt = userDao.insertUser(user);
+
+        assertTrue(rowCnt!=0);
+    }
+
+    @Test
+    public void selectUserTest() throws Exception {
+        userDao.deleteAllUser();
+        User user = new User("gd095529","asdf",null,"asdf","asdf");
+        userDao.insertUser(user);
+        User user1 = userDao.selectUser("gd095529");
+
+        assertTrue(user1.getId().equals("gd095529"));
     }
 }
